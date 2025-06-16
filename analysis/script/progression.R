cb5b38aedf4d79e370c5d807c92fbc401b0928b2
alexpaynter <alexcpaynter@gmail.com>
2025-06-15 18:21:08 -0700

    Stopping point, progression script is incomplete but dinner needs to be made

--- analysis/script/progression.R
+++ analysis/script/progression.R
@@ -5,9 +5,6 @@ library(purrr)
 library(here)
 purrr::walk(.x = fs::dir_ls(here("R")), .f = source)
 
-reg <- readr::read_csv(
-  here('data-raw', 'PANC', 'regimen_cancer_level_dataset.csv')
-)
 med_onc <- readr::read_csv(
   here('data-raw', 'PANC', 'med_onc_note_level_dataset.csv')
 )
@@ -39,6 +36,20 @@ status_sum %<>%
 
 lot <- readr::read_rds(here('data', 'drug', 'lot.rds'))
 
+reg <- readr::read_csv(
+  here('data-raw', 'PANC', 'regimen_cancer_level_dataset.csv')
+)
+
+cli_abort("Need to create the regimen start variable (reference the newer data guides) and apply that to the line of therapy data before splitting into first and second lines.  Then mark the first therapy switch using the second line data, merge in the other stuff, and you can find the people who progressed within 6 weeks to 6 months.  How ghastly this all is...")
+
+lot |> 
+  select(
+
+# There are lots of people in ehre who won't be in our final cohort, but that's OK.
 first_lines <- lot |>
   filter(line_of_therapy %in% 1) |>
-  
+  filter(gem_based | fluoro_based)
+
+second_lines <- lot |>
+  filter(record_id %in% first_lines$record_id) |>
+  filter(line_of_therapy %in% 2)
