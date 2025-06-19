1193c4d977263a149ed25cd47daeb5200d87f6db
alexpaynter <alexcpaynter@gmail.com>
2025-06-19 09:37:39 -0700

    removed duplicate lines

---
+++ analysis/explore/duplicate_line_check.R
@@ -0,0 +1,26 @@
+# builds on create_lines.R - run that first and this may work.  but it's exploratory so no whining if not.
+
+cohort_prog_not_verified <- readr::read_rds(
+  here('data', 'cohort_prog_not_verified.rds')
+)
+
+rel_group <- cohort_prog_not_verified %>%
+  select(record_id, index_line)
+
+line_small <- line_of_ther %>%
+  filter(record_id %in% rel_group$record_id) %>%
+  left_join(., rel_group, by = 'record_id') %>%
+  filter(line_of_therapy <= index_line)
+
+line_small %>%
+  group_by(record_id) %>%
+  mutate(
+    prev_reg_drugs = lag(regimen_drugs),
+    dup_reg = prev_reg_drugs == regimen_drugs,
+    any_dup = any(dup_reg, na.rm = T)
+  ) %>%
+  ungroup(.) %>%
+  filter(any_dup) %>%
+  select(record_id, line_of_therapy, regimen_drugs, prev_reg_drugs) %>%
+  arrange(record_id, line_of_therapy) %>%
+  View(.)
